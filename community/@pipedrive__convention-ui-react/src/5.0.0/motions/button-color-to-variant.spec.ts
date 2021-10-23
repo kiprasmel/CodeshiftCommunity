df38ac3b4889956393a4651f5a3c34b4cf129448
Kipras Melnikovas <kipras@kipras.org>
2021-10-24 02:31:14 +0300

    cui: buttonColorToVariant: add more granular test cases
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- community/@pipedrive__convention-ui-react/src/5.0.0/motions/button-color-to-variant.spec.ts
+++ community/@pipedrive__convention-ui-react/src/5.0.0/motions/button-color-to-variant.spec.ts
@@ -4,7 +4,94 @@ import { buttonColorToVariant } from "./button-color-to-variant";
 describe("@pipedrive/convention-ui-react@5.0.0 buttonColorToVariant motion", () => {
     inlineTest({
         transformer: buttonColorToVariant,
-        should: "convert CUI Buttons from color to variant, BUT should NOT convert non-CUI Buttons",
+        should: "convert regularly-imported CUI Button from color to variant",
+        from: `
+import { Button } from "@pipedrive/convention-ui-react";
+
+const btn = <Button
+	color="ghost"
+	size="s"
+>
+	hi
+</Button>
+
+		`,
+        to: `
+import { Button } from "@pipedrive/convention-ui-react";
+
+const btn = <Button
+	variant="ghost"
+	size="s"
+>
+	hi
+</Button>
+
+		`,
+    });
+
+    inlineTest({
+        transformer: buttonColorToVariant,
+        should: "convert alias-like-imported CUI Button from color to variant",
+        from: `
+import { Button as CUIButton } from "@pipedrive/convention-ui-react";
+
+const btn = <CUIButton
+	color="ghost"
+	size="s"
+>
+	hi
+</CUIButton>
+
+		`,
+        to: `
+import { Button as CUIButton } from "@pipedrive/convention-ui-react";
+
+const btn = <CUIButton
+	variant="ghost"
+	size="s"
+>
+	hi
+</CUIButton>
+
+		`,
+    });
+
+    inlineTest({
+        transformer: buttonColorToVariant,
+        should: "NOT convert a non-CUI regularly-imported Button from color to variant",
+        fromTo: `
+import { Button } from "@someone/cool-repo";
+
+const btn = <Button
+	color="ghost"
+	size="s"
+>
+	hi
+</Button>
+
+		`,
+    });
+
+    inlineTest({
+        transformer: buttonColorToVariant,
+        should: "NOT convert a non-CUI alias-like-imported Button from color to variant",
+        fromTo: `
+import { Button as ActuallyNotCUIButton } from "@someone/cool-repo";
+
+const btn = <ActuallyNotCUIButton
+	color="ghost"
+	size="s"
+>
+	hi
+</ActuallyNotCUIButton>
+
+		`,
+    });
+
+    inlineTest({
+        transformer: buttonColorToVariant,
+        should:
+            "convert CUI Buttons from color to variant, even when wrapped with styled()``, BUT should NOT convert non-CUI Buttons",
         from: `
 import { styled } from "styled";
 import { NotCUIButton } from "somewhere-else";
