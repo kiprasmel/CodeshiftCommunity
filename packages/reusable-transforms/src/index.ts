be9f8c3bf061f436999151b8509712cc387ed169
Kipras Melnikovas <kipras@kipras.org>
2021-11-30 06:27:14 +0200

    feat: initialize `reusable-transforms` package
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

---
+++ packages/reusable-transforms/src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * this file is needed to pass the damn preconstruct check
+ * even when it should not be necessary...
+ */
+
+/**
+ * we're not exporting anything here,
+ * use the path of the transform
+ */
+export {};
